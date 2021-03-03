5b1be6ad65656f8f49153eae2038a5a51a2cf2ca
Ed Abbey <websterhomer78@gmail.com>
2021-03-03 08:48:18 -0700

    starting to learn more about JSON data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 
-fetch("https://api.openweathermap.org/data/2.5/forecast?q={input.value}&appid=8142ee2395c021b19a84e0110a877948")
 
+fetch("http://api.openweathermap.org/data/2.5/weather?q=salt%20lake%20city&appid=59bf5c5c074ea67df6a177d56a2d3930")
 .then(function(response) {
     return response.json();
-})
-.then(function(data) {
+  })
+  .then(function(data) {
     console.log(data);
-});
+    var icon = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+    console.log(icon);
+  });
