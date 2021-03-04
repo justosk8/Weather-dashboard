1f61bfe4fccb1f15639686db8ac182d5074b96b7
Ed Abbey <websterhomer78@gmail.com>
2021-03-04 15:44:08 -0700

    starting over

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
-
-
+/*
 fetch("http://api.openweathermap.org/data/2.5/weather?q=salt%20lake%20city&appid=59bf5c5c074ea67df6a177d56a2d3930")
 .then(function(response) {
     return response.json();
@@ -9,3 +8,9 @@ fetch("http://api.openweathermap.org/data/2.5/weather?q=salt%20lake%20city&appid
     var icon = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
     console.log(icon);
   });
+
+  function display() {
+      document.appendChild(".icon", icon);
+  }
+  */
+ 
