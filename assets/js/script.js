3daead68032a00f4a08b72573297ea5dd3b5b18f
Ed Abbey <websterhomer78@gmail.com>
2021-03-04 19:30:01 -0700

    js complete great tutorial

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,58 @@
-/*
-fetch("http://api.openweathermap.org/data/2.5/weather?q=salt%20lake%20city&appid=59bf5c5c074ea67df6a177d56a2d3930")
-.then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    console.log(data);
-    var icon = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-    console.log(icon);
-  });
+const api = {
+  key: "59bf5c5c074ea67df6a177d56a2d3930",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
-  function display() {
-      document.appendChild(".icon", icon);
+const searchbox = document.querySelector(".search-box");
+searchbox.addEventListener("keypress", setQuery);
+
+function setQuery(event) {
+  if (event.keyCode ===13) {
+    getResults(searchbox.value);
+    console.log(searchbox.value);
   }
-  */
+}
+
+function getResults(query) {
+  fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
+  .then(weather => {
+    return weather.json();
+  }).then(displayResults);
+}
+
+function displayResults(weather) {
+  console.log(weather);
+  let city = document.querySelector(".location .city");
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  let now = new Date();
+  let date = document.querySelector('.location .date');
+  date.innerText = dateBuilder(now);
+
+  let temp = document.querySelector(".current .temp");
+  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°f</span>`;
+
+  let weatherEl = document.querySelector(".current .weather");
+  weatherEl.innerText = weather.weather[0].main;
+
+  let hilow = document.querySelector(".hi-low");
+  hilow.innerText = `${Math.round(weather.main.temp_min)}°f /${Math.round(weather.main.temp_max)}°f`;
+}
+
+function dateBuilder(d) {
+  let months = [
+    "January", "February", "March", "April", "May", "June", "July", "August", 
+    "September", "October", "November", "December"
+  ];
+  let days = [
+    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+  ];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`;
+}
  
